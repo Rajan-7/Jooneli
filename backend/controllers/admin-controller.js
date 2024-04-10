a85300cedb1dd2d65163f7cbff603a11a32052ce
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-04-10 14:16:51 +0545

    ft:modification admin-controller.js,admin-router.js

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -1,19 +1,48 @@
-const connectDb = require('../utils/db');
+const connectDb = require("../utils/db");
 
-const getAllUser = async(req,res)=>{
-    try {
-      const conn = await connectDb();
-      conn.query("SELECT * FROM users",(err,rows)=>{
-        if(err){
-         return res.status(500).json({message:"Internal server error"});
-        }else{
-         return res.status(200).json(rows);
-        }
-      })
-    } catch (error) {
-      console.log(error);
-    }
-}
+// To Fetch All The Users Data
+const getAllUser = async (req, res) => {
+  try {
+    const conn = await connectDb();
+    conn.query("SELECT * FROM users", (err, rows) => {
+      if (err) {
+        return res.status(500).json({ message: "Internal server error" });
+      } else {
+        return res.status(200).json(rows);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+// To Fetch Single User Data By Id
+const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const conn = await connectDb();
+    conn.query("SELECT id,username,email,contact FROM users WHERE id=?", [id],(err,rows)=>{
+      if(err){
+        return res.status(500).json({error:"Internal server error"});
+      }else{
+        return res.status(200).json(rows[0]);
+      }
+    });
+  } catch (error) {
+    console.log(`Error While Fetching Single User:${error}`);
+  }
+};
 
-module.exports = getAllUser
+// TO Delete Users By id
+const deleteUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const conn = await connectDb();
+    conn.query("DELETE FROM users WHERE id = ?", [id]);
+    return res.status(200).json({ Message: `User Deleted Successfully` });
+  } catch (error) {
+    console.log(`Error While Deleting Users:${error}`);
+  }
+};
+
+module.exports = { getAllUser, deleteUserById, getUserById };
--- backend/router/admin-router.js
+++ backend/router/admin-router.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authMiddleware = require('../middleware/auth-middleware');
 const adminMiddleware = require('../middleware/admin-middleware')
-const getAllUser = require('../controllers/admin-controller');
+const {getAllUser,deleteUserById,getUserById} = require('../controllers/admin-controller');
 
 router.route('/users').get(authMiddleware,adminMiddleware,getAllUser);
+router.route('/users/:id').get(authMiddleware,adminMiddleware,getUserById);
+router.route('/users/delete/:id').delete(authMiddleware,adminMiddleware,deleteUserById)
 
 module.exports =router
